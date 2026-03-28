9a9c6edb640738cff38a66c8c030dac7437ad8bd
Justino José da Silva <justinojosesilva@gmail.com>
2026-03-27 23:16:09 -0300

    fix to lint

--- apps/web/components/property/property-page-client.tsx
+++ apps/web/components/property/property-page-client.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import Link from 'next/link'
 import { Plus } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
